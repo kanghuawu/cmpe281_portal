989ee587b15717c44f0942d61578aeb8b24434b3
ramyamariappan <ramya.mariappan@sjsu.edu>
2017-04-30 23:10:44 -0700

    changing kong endpoints and routes

--- portal/routes/index.js
+++ portal/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
-var address = 'http://54.215.214.142:9090/v3/starbucks/order'
+var address = 'http://54.193.44.42:8000'
+
 
 router.get('/', function(req, res, next) {
 	res.render('index');
@@ -10,6 +11,8 @@ router.get('/', function(req, res, next) {
 router.get('/stores', function(req, res, next) {
 	res.render('enterOrder');
 })
+
+
 
 router.post('/placeOrder', function(req, res, next) {
 	console.log(req.body);
@@ -20,15 +23,21 @@ router.post('/placeOrder', function(req, res, next) {
 				"qty": req.body.qty,
 				"name": req.body.name,
 				"milk": req.body.milk,
-				"size": req.body.size,
-				//"store": req.body.shopname
+				"size": req.body.size
 			}
 		]
 	};
-	request.post(
-		address,
-		{ json: order },
-		function (error, response, body) {
+	var hostname = (req.body.shopname=='SF')? "portal.com" : "sj.com";
+	console.log("hostname"+ hostname);
+	var options = {
+	  method: 'post',
+	  json: order,
+	  url: address + "/"+req.body.shopname + "/starbucks/order",
+	  headers: {
+	    'Host': hostname
+	  }
+	}
+	request(options,function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				console.log("response::::: "+response.body.id);
 				res.redirect('order/' + response.body.id);
