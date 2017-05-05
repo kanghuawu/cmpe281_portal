e7c55adf6f3828f74639afbc766f6f7e0376677d
tuanungquoc <tuanungquoc@gmail.com>
2017-05-04 23:11:25 -0700

    Refactor the code for final checkin

---
+++ portal/config/data.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    'size' : ['Large','Medium','Small'],
+    'milk' : ['Whole Milk','2% Milk','1% Milk'],
+    'name' : ['Latte','Mocha'],
+    'location':['Here','Takeout'],
+    'stores': [{'name':'San Francisco','code':'SF'},{'name':'San Jose','code':'SJ'}],
+    'qty': ['1','2','3','4','5']
+
+};
---
+++ portal/config/hostname.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'address' : 'http://54.193.44.42:8000'
+
+};
